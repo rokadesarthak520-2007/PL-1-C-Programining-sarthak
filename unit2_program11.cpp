0cca4263f35d47d8bc3fc28c41ff7898cfcfb499
rokadesarthak520-2007 <rokadesarthak520@gmail.com>
2026-09-24 19:07:50 +0530

    Create unit2_program11.cpp

---
+++ unit2_program11.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Even your number:");
+    scanf("%d", &num);
+
+    if(num%2 == 0)
+    {
+        printf("The number is Even.");
+    }
+    else
+    {
+        printf("The number is odd.");
+    }
+
+    return 0;
+}
