a17d7c6898f41d9976e470620f3377f18a52569a
rokadesarthak520-2007 <rokadesarthak520@gmail.com>
2026-09-24 18:47:13 +0530

    Create unit1_program4.cpp

---
+++ unit1_program4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius, area;
+
+    printf("\n Enter the radius of the circle : ");
+    scanf("%f", &radius);
+    area = (3.14 * radius * radius);
+    printf("\n Area of circle = %f", area);
+
+    return 0;
+}
